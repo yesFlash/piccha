6f38f882b7c71bf7a587cd10c1e5c93af365de2e
littlebird <dlwldnjs950@naver.com>
2024-03-05 22:40:29 +0900

    initial commit

---
+++ server/src/main/java/com/littlebird/piccha/PicchaApplication.java
@@ -0,0 +1,13 @@
+package com.littlebird.piccha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicchaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/littlebird/piccha/PicchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlebird.piccha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
